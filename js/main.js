7b6fa6359a27d0879d07e19f85ec3c1e8d80f0ad
Situ William <situ_william@yahoo.com>
2017-12-21 14:47:50 -0500

    finished adding event listeners

---
+++ js/main.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded',function(e){
+	var signIn = document.querySelector('.signin');
+	var modal = document.querySelector('.modal');
+	var close = document.querySelector('.close');
+	var submit = document.querySelector('.submit');
+	var inputs = document.querySelectorAll('input');
+	signIn.addEventListener('click',function(){
+		modal.style.display = 'block';
+	});
+
+	close.addEventListener('click',function(){
+		modal.style.display = 'none';
+	});
+
+	submit.addEventListener('click',function(eventObject){
+		eventObject.stopPropagation();
+		eventObject.preventDefault();
+		inputs.forEach(function(input){
+			input.classList = 'error'
+		});
+	});
+
+	inputs.forEach(function(input){
+		input.addEventListener('click',function(eventObject){
+			eventObject.stopPropagation();
+		});
+	});
+
+	modal.addEventListener('click',function(){
+		modal.style.display = 'none';
+	});
+});
